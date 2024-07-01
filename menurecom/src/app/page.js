a0084cb36efec7f058bdd2ce307905505bf4a329
Juna Kim <kimjuna0118@gmail.com>
2024-07-01 19:58:13 -0400

    set up

---
+++ menurecom/src/app/page.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Page() {
+  return (
+    <div className="Page">
+      <header className="App-header">
+        <p>
+         hello world
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default Page;
